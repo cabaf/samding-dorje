d435dc0a352ee636e3efc0308644da6d0335e219
Fabian Caba Heilbron <cabaf@localhost.localdomain>
2014-06-02 23:56:03 +0300

    Added basic functions for reading datasets and extracting visual features.

---
+++ code/datasetIO.py
@@ -0,0 +1,81 @@
+'''
+   _____________________________________________________________________
+   datasetIO.py
+   ====================
+   Includes several functions related with accesing dataset info.
+
+   Supported datasets:
+     - Hollywood2
+     - HMDB51
+     - Olympic Sports
+
+   Copyright @ Fabian Caba H.
+   _____________________________________________________________________
+'''
+import os.path
+import sys
+import numpy as np
+from scipy.io import loadmat
+
+def get_all_video_ids(dataset_info_file, dataset_path, **args):
+    """
+    ____________________________________________________________________
+       get_all_video_ids:
+         Search and return all video ids on a dataset. Useful for 
+         extracting features.
+         args:
+           dataset_info_file: full path of matfile containing info about
+           the train/set splits on the dataset. It must have the
+           following struct:
+             data(set_id).train.(className)
+             data(set_id).test.(className)
+           dataset_path: full path for the dataset root directory.
+         return:
+           full_path_video_list: returns ids for the entire dataset in 
+             a string list.
+    ____________________________________________________________________
+    """
+    ############################################################################
+    opts = {"verbose":False}
+    if len(args)==1 and isinstance(args[0], dict):
+        opts.update(args[1])
+    if not os.path.isfile(dataset_info_file):
+        print "Error: Dataset file info not found."
+        sys.exit(0)
+
+    dataset_info = loadmat(dataset_info_file)["data"][0]
+    n_sets = len(dataset_info)
+    if opts["verbose"]:
+        print "Getting al video ids..."
+    video_list = []
+    for set_id in range(n_sets):
+        this_train_set = loadmat(dataset_info_file)["data"][0][set_id]\
+                                                   ["train"][0][0]
+        this_test_set = loadmat(dataset_info_file)["data"][0][set_id]\
+                                                  ["test"][0][0]
+        this_video_list = np.empty((1, 1))
+        for action_id in this_train_set.dtype.names:
+            this_video_list = np.vstack((this_video_list, 
+                                         this_train_set[action_id]))
+            this_video_list = np.vstack((this_video_list, 
+                                         this_test_set[action_id]))
+        this_video_list = np.delete(this_video_list, 0, 0)
+        this_video_list = np.unique(this_video_list)
+        video_list += list(this_video_list)
+    video_list = np.unique(np.array(video_list))
+    full_path_video_list = []
+    for vidx in video_list:
+        print vidx
+        full_path_video_list += get_video_full_path(vidx, dataset_path)
+    ############################################################################
+    return full_path_video_list
+    ############################################################################
+
+def get_video_full_path(video_id, dataset_path, ext="avi"):
+    command = "find {0} -name '*{1}.{2}'".format(dataset_path, video_id, ext)
+    file_path = os.popen(command).read()[:-1]
+    # Sanity check
+    if not os.path.isfile(file_path):
+        print "Error: File not found. Check provided dataset path."
+        file_path = ''
+    return file_path
---
+++ code/featureExtraction.py
@@ -0,0 +1,99 @@
+'''
+   _____________________________________________________________________
+   featureExtraction.py
+   ====================
+   Includes several functions related with the feature extraction stage.
+
+   Copyright @ Fabian Caba H.
+   _____________________________________________________________________
+'''
+
+import os
+import os.path
+import subprocess
+from multiprocessing import Pool
+import itertools
+
+def extract_features(video_files, output_path, idt_bin, **args):
+    """
+    ____________________________________________________________________
+       extract_features:
+         Calls binary for computing improved trajectories on a 
+         video list.
+         args:
+           video_files: List of full path of video files.
+           output_path: Where will be located the output trajectories.
+           idt_bin: Improved dense trajectories binary full path.
+           args(optional): It should contain the following dict 
+             dictionary key/value:
+             "trajectory_length": Length of tracking trajectries.
+               (default: L=15)
+             "sampling_stride": the stride for dense sampling feature 
+               points. (default: W=5 pixels)
+             "neighborhood_size": The neighborhood size for computing 
+               the descriptor. (default: N=32 pixels)
+             "spatial_cells": The number of cells in the nxy axis.
+               (default: nxy=2 cells)
+             "temporal_cells": The number of cells in the nt axis. 
+               (default: nt=3 cells)
+             "verbose": Display information (True/False).
+             "n_jobs": Number of jobs for running in parallel. 
+               (default: n_jobs=2)
+         return:
+           failed_computation: List containing files which feature 
+             extraction fails.
+    ____________________________________________________________________
+    """
+    ############################################################################
+    opts = {"trajectory_length":15, "sampling_stride":5, "neighborhood_size":32,
+            "spatial_cells":2, "temporal_cells":3, "verbose":False,
+            "n_jobs": 2}
+    if args is not None:
+        if isinstance(args, dict):
+            opts.update(args)
+        else:
+            print "Error: Third argument should be a dictionary."
+            sys.exit(0)
+    failed_computation = []
+    if not os.path.isfile(idt_bin):
+        print "Error: Improved dense trajectory binary not found."
+        sys.exit(0)
+    if not os.path.isdir(output_path):
+        if opts["verbose"]:
+            print "Creating directory: {}".format(output_path)
+        os.mkdir(output_path)
+    if opts["verbose"]:
+        print "# Feature Extraction: Improved Trajectories using\
+               Fundamental Matrix."
+    idt_args = itertools.izip(video_files, itertools.repeat(output_path),
+                             itertools.repeat(idt_bin), itertools.repeat(opts))
+    f_pool = Pool(opts["n_jobs"])
+    for idx, failed_computation in enumerate(\
+      f_pool.imap_unordered(daemon_improved_trajectories_call, idt_args),1):
+        sys.stderr.write('\rPercentage video processed: %0.2f%%' % \
+                                              (100*idx/(0.0+len(video_files))))
+    print "\nFinish!\n"
+    f_pool.close()
+    f_pool.join()
+    ############################################################################
+    return failed_computation
+    ############################################################################
+
+def improved_trajectories_call(video_file, output_path, idt_bin, opts):
+    if not os.path.isfile(video_file):
+        if opts["verbose"]:
+            print "Failed computation: {}".format(video_file)
+        return video_file
+    command = "{0} {1} -L {2} -W {3} -N {4} -s {5} -t {6} -o {7}".format(
+              idt_bin, video_file, opts["trajectory_length"],
+              opts["sampling_stride"], opts["neighborhood_size"],
+              opts["spatial_cells"], opts["temporal_cells"], output_path)
+    print command
+    os.system(command)
+    if opts["verbose"]:
+        print "Processed video {}".format(video_file)
+    return None
+
+def daemon_improved_trajectories_call(args):
+    return improved_trajectories_call(*args)
+
